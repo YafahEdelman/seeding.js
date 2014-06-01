514a7d086013a89f11037e11fbebe3a06d3f63d1
JacobEdelman <edelman.jd@gmail.com>
2014-06-01 19:14:06 -0400

    added peer api key

--- public/client.js
+++ public/client.js
@@ -1,19 +1,11 @@
 var peerID=Math.random().toString(36).substring(7);
 
 
-var peer = new Peer(peerID, {key: 'myapikey'}); //needs changing
+var peer = new Peer(peerID, {key: 'a96mry70om5kx1or'}); //needs changing
 vals={}
 peer.on('connection', function(conn) {
-  conn.send('open');
   conn.on('data', function(key){
-    conn.vals.key
-    // Will print 'hi!'
-    console.log(data);
-  });
-  conn.on('data', function(data){
-
-    // Will print 'hi!'
-    console.log(data);
+    conn.send(vals[key]);
   });
 });
 
@@ -27,12 +19,9 @@ function getItem(key,callback=function(x){},error=function(x){}){
   socket.on('put key holder', function (msg) {
       var conn = peer.connect(msg.peerID);
       conn.send(msg.key);
-      conn.on('open', function(){
-        conn.send('key',msg.key);//all are class for the stuff
-      });
       conn.on('data', function(data){
         if(msg.hash===hash(data)){
-          vals.key=data;
+          vals[key]=data;
           callback(data);
           return data;
         }else{
@@ -42,7 +31,7 @@ function getItem(key,callback=function(x){},error=function(x){}){
       //what to do with reply is said later
   });
   socket.on('put data', function (msg) {
-    vals.key=data;
+    vals[key]=data;
     callback(msg.val);
     return msg.val;
   });
